49e880a6c902969cc69cbd31e815370c825b9ef9
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-11-23 20:01:10 +0100

    feat: implement divide function with input validation and add corresponding tests

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,34 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+  describe("Function - divide", () => {
+    // Each `it` block represents 1 test. You can use the following as a template:
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
 
-    })    
-})
+    it("should take two arguments", () => {
+      expect(divide.length).toBe(2);
+    });
 
-*/
+    it("should return the division of the two numbers", () => {
+      expect(divide(2, 4)).toEqual(0.5);
+      expect(divide(3, 4)).toEqual(0.75);
+      expect(divide(9, 3)).toEqual(3);
+      expect(divide(4, 2)).toEqual(2);
+    });
+
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(divide(1)).toEqual(undefined);
+      expect(divide()).toEqual(undefined);
+      expect(divide(undefined, 1)).toEqual(undefined);
+    });
+
+    it("should return undefined if any of the arguments is not a number", () => {
+      expect(divide(" ", " ")).toEqual(undefined);
+      expect(divide([1], [2])).toEqual(undefined);
+      expect(divide({ a: 1 }, { b: 2 })).toEqual(undefined);
+      expect(divide(true, false)).toEqual(undefined);
+    });
+  });
+});
